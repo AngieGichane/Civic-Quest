8b2bb3f937e86d05669bd265a67c38abc978dac8
Angela Gichane <159881265+AngieGichane@users.noreply.github.com>
2025-07-04 17:23:39 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+     base: '/Civic-Quest/,
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
